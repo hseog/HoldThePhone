d4acaf7299d5fe3a433d160842cb66d92654ddd0
eugenet12 <eugenet@princeton.edu>
2014-01-29 15:23:20 -0500

    Add kaiseki. Add basic routes.

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ var path = require('path');
 var app = express();
 
 // environment
+// Set port 3000
 app.set('port', process.env.PORT || 3000);
+// Set views variable to '/views'
 app.set('views', __dirname + '/views');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+// Allows parsing of body
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+// Sets homepage
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.compress());
 
@@ -25,21 +29,14 @@ if ('development' == app.get('env')) {
    app.use(express.errorHandler());
 }
 
-app.get('/', function (req, res) {
-    res.send('<h1>Homepage</h1>');
-});
-
-app.get('/about', function(req, res) {
-    res.send('<h2>about me</h2>');
-});
-
 // Test ejs
-app.get('/helloworld/:name', routes.helloworld);
-app.post('/helloworld/result', routes.helloworld_result)
+//app.get('/helloworld/:name', routes.helloworld);
+//app.post('/helloworld/result', routes.helloworld_result)
 
-app.get('/name/:myname', function(req, res) {
-    res.send('My name is ' + req.params.myname);
-});
+app.get('/joinroom', routes.joinroom);
+app.post('/joinroom/result', routes.joinroom_result);
+app.get('/newroom', routes.newroom);
+app.post('/newroom/result', routes.newroom_result);
 
 http.createServer(app).listen(app.get('port'), function(){
       console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -11,3 +11,21 @@ exports.helloworld = function(req, res){
 exports.helloworld_result = function(req, res) {
 	res.render('helloworld_result.ejs', {classroom:req.body.classroom, password:req.body.password});
 }
+
+exports.joinroom = function(req, res) {
+	res.render('joinroom.ejs');
+}
+
+exports.joinroom_result = function(req, res) {
+	// get from database
+	res.render('joinroom_result.ejs', {roomname:req.body.roomname});
+}
+
+exports.newroom = function(req, res) {
+	res.render('newroom.ejs');
+}
+
+exports.newroom_result = function(req, res) {
+	// save in database
+	res.render('newroom_result.ejs', {roomname:req.body.roomname});
+}
