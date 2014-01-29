417a2372588652b6fbc72287a120261794c0d576
Heemin Seog <seog.heemin@gmail.com>
2014-01-29 00:27:21 -0500

    preliminary db setup

--- models/db.js
+++ models/db.js
@@ -1,1 +1,5 @@
-mongodb://shet:seog1122@ds027829.mongolab.com:27829/holdthephone
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://hseog:seog1122@ds027829.mongolab.com:27829/holdthephone');
+
+module.exports = mongoose.connection;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+
+var Schema = require('../schemas/room');
+
+module.exports = function (confused) {
+    
+    var functions = {};
+
+    functions.confused = function(req, res) {
+	
+    }
+}
